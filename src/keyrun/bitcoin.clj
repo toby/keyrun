f6f5cca5a07938414fdbdb08e9fdf41a9d5c1620
Toby Padilla <tobypadilla@gmail.com>
2015-11-12 10:50:12 -0800

    bitcoin.clj

---
+++ src/keyrun/bitcoin.clj
@@ -0,0 +1,118 @@
+(ns keyrun.bitcoin
+  (:require [clojure.tools.logging :as log]
+            [com.stuartsierra.component :as component])
+  (:import
+    (org.bitcoinj.core NetworkParameters
+                       Address
+                       ECKey
+                       PeerGroup
+                       BlockChain
+                       Utils
+                       PeerFilterProvider
+                       BloomFilter
+                       AbstractPeerEventListener
+                       AbstractBlockChainListener
+                       DownloadProgressTracker)
+    (org.bitcoinj.store SPVBlockStore)
+    (org.bitcoinj.net.discovery DnsDiscovery)
+    (org.bitcoinj.params TestNet3Params RegTestParams MainNetParams)))
+
+(defmulti network-params identity)
+(defmethod network-params "testnet" [_] (TestNet3Params/get))
+(defmethod network-params "regtest" [_] (RegTestParams/get))
+(defmethod network-params :default [_] (MainNetParams/get))
+
+(defmulti file-prefix class)
+(defmethod file-prefix TestNet3Params [_] "testnet")
+(defmethod file-prefix RegTestParams [_] "regtest")
+(defmethod file-prefix MainNetParams [_] "production")
+
+(defn string->Address [address params]
+  (try
+    (Address. params address)
+    (catch Exception e
+      (log/error "Bad address:" (.getMessage e)))
+    ))
+
+(defn address-peer-filter [addresses]
+  (reify
+    PeerFilterProvider
+    (beginBloomFilterCalculation [this]
+      (log/info "Begin bloom filter calculation"))
+    (endBloomFilterCalculation [this]
+      (log/info "End bloom filter calculation"))
+    (getBloomFilter [this size falsePositiveRate nTweak]
+      (log/info "get bloom filter")
+      (let [bloom (BloomFilter. size falsePositiveRate nTweak)]
+        (doseq [address addresses]
+          (.insert bloom (.getHash160 address)))
+        bloom))
+    (getBloomFilterElementCount [this] (count addresses))
+    (getEarliestKeyCreationTime [this] 0)
+    (isRequiringUpdateAllBloomFilter [this] false)))
+
+(defn peer-event-listener [params]
+  (proxy [AbstractPeerEventListener] []
+    (onTransaction [peer transaction]
+      (log/info "PEER TRANSACTION:" (.getHash transaction))
+      (doseq [output (.getOutputs transaction)]
+        (log/info "OUTPUT value:"  (.getValue output))
+        (log/info "OUTPUT:"  (.toString output)))
+      )))
+
+(defn blockchain-event-listener [params]
+  (proxy [AbstractBlockChainListener] []
+    (isTransactionRelevant [transaction]
+      (log/info "relevant?" transaction))
+    (notifyTransactionIsInBlock [tx-hash block block-type relativity-offset]
+      (log/info "TRANSACTION" tx-hash "is in block" block))
+    (receiveFromBlock [transaction block block-type relativity-offset]
+      (log/info "BLOCK TRANSACTION:" (.getHash transaction))
+      (doseq [output (.getOutputs transaction)]
+        (log/info "OUTPUT value:"  (.getValue output))
+        (log/info "OUTPUT:"  (.toString output)))
+      )))
+
+(defn download-progress-tracker []
+  (proxy [DownloadProgressTracker] []
+    (onChainDownloadStarted [peer blocks-left]
+      (log/info "Start download of" blocks-left "from blockchain."))
+    (doneDownload []
+      (log/info "Done downloading blocks."))
+    (progress [percent blocks-so-far date]
+      (log/info "Downloaded" (str percent "%") "of" blocks-so-far "blocks."))
+    (onBlocksDownloaded [peer block filtered-block blocks-left]
+      (when (= 0 (mod blocks-left 1000))
+        (log/info blocks-left "blocks left.")))))
+
+(defrecord BitcoinServer [network-type namespace-address]
+  component/Lifecycle
+  (start [this]
+    (log/info "Starting bitcoin")
+    (let [params (network-params network-type)
+          network-prefix (file-prefix params)
+          address (string->Address namespace-address params) ; TODO check nil
+          peer-filter (address-peer-filter [address])
+          peer-listener (peer-event-listener params)
+          blockchain-listener (blockchain-event-listener params)
+          blockstore-file (clojure.java.io/file (str "./" network-prefix ".blockstore"))
+          blockstore (SPVBlockStore. params blockstore-file) ; TODO load checkpoint
+          blockchain (BlockChain. params blockstore)
+          peer-group (PeerGroup. params blockchain)]
+      (.addListener blockchain blockchain-listener)
+      (log/info "Starting peer group...")
+      (doto peer-group
+        (.setUserAgent "key.run", "0.1")
+        (.addPeerDiscovery (DnsDiscovery. params))
+        (.addPeerFilterProvider peer-filter)
+        (.clearEventListeners)
+        (.addEventListener peer-listener)
+        (.addEventListener (download-progress-tracker))
+        ; (.setFastCatchupTime) ; TODO set to start of key.run
+        (.start)
+        (.downloadBlockChain))
+      (log/info "Done downloading blockchain")
+      this))
+  (stop [this]
+    (log/info "Stopping bitcoin")
+    this))
