ddaf97028280722caef6b5c84c79c5ebddf09008
Toby Padilla <tobypadilla@gmail.com>
2015-12-03 14:32:28 -0800

    version++

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject keyrun "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject keyrun "0.2.0"
+  :description "key.run server"
+  :url "http://key.run"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
